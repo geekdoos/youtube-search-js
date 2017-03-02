a8018dac9a287ce25d6cf4df80cf9efd2446b53f
KHACHIAI Oussama <oussama.khachiai@gmail.com>
2017-03-02 23:21:12 +0000

    fix some issues

--- js/api.js
+++ js/api.js
@@ -12,33 +12,7 @@ $(document).ready(function() {
         youtubeApiCall();
         return false;
     });
-    jQuery( "#geekdoos-search" ).autocomplete({
-        source: function( request, response ) {
-            //console.log(request.term);
-            var sqValue = [];
-            jQuery.ajax({
-                type: "POST",
-                url: "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1",
-                dataType: 'jsonp',
-                data: jQuery.extend({
-                    q: request.term
-                }, {  }),
-                success: function(data){
-                    console.log(data[1]);
-                    obj = data[1];
-                    jQuery.each( obj, function( key, value ) {
-                        sqValue.push(value[0]);
-                    });
-                    response( sqValue);
-                }
-            });
-        },
-        select: function( event, ui ) {
-            setTimeout( function () {
-                youtubeApiCall();
-            }, 300);
-        }
-    });
+
     function getVideoDetails(ids){
         $.ajax({
             cache: false,
